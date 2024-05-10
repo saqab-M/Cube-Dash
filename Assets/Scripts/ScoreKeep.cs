c48e1a4cc90492e8a2df25fafca6ea2e6590b8da
saqib <47401181+saqab-M@users.noreply.github.com>
2024-05-10 21:09:11 +0100

    High score
    keep high score

--- Assets/Scripts/ScoreKeep.cs
+++ Assets/Scripts/ScoreKeep.cs
@@ -7,12 +7,18 @@ public class ScoreKeep : MonoBehaviour
 {
 
     public TextMeshProUGUI scoreTxt;
+    public TextMeshProUGUI highScoreTxt;
 
     int score = 0;
+    int highScore = 0;
+
+    private const string HIGH_SCORE_KEY = "HighScore";
 
     private void Start()
     {
+        highScore = PlayerPrefs.GetInt(HIGH_SCORE_KEY,0);
         UpdateScore();
+        UpdateHighScore();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,9 +33,22 @@ public class ScoreKeep : MonoBehaviour
     {
         score = score + points;
         UpdateScore();
+
+        //save high score if its higher that the current stored
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt(HIGH_SCORE_KEY, highScore);
+            PlayerPrefs.Save(); // save changes to player prefs
+        }
     }
     private void UpdateScore() 
     {
         scoreTxt.text = score.ToString();
+    }
+
+    private void UpdateHighScore() 
+    { 
+        highScoreTxt.text = "High Score: " + highScore.ToString();
     }
 }
